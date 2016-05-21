bec8f0bc409a17871df685016a293f61d7c90126
geduardcatalin <g.eduardcatalin@gmail.com>
2016-05-21 18:28:24 +0300

    simple $_GET example

--- index.php
+++ index.php
@@ -4,6 +4,22 @@
 	<title>Document</title>
 </head>
 <body>
-	<?php echo "hello-world"; ?>
+	<form action="index.php" method="GET">
+		<p>Name:<input type="text" name="username"></p>
+		<p>Password:<input type="text" name="password"></p>
+		<p><input type="submit" value="SUBMIT"></p>
+	</form>
+	<?php
+		$username = $_GET["username"];
+		$password = $_GET["password"];
+
+		if (isset($username) && isset($password)){
+			if(!empty($username) && !empty($password)){
+				echo "Your username is <b>".$username."</b> and your password is <b>".$password."</b>";
+			} else {
+				echo "You didn't enter anything!";
+			} 
+		}
+	?>
 </body>
 </html>
